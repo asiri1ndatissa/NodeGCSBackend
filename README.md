acebee440213ffcccb49981526bb79a4c6b54057
Xasiri <asiri1234@gmail.com>
2021-02-07 18:41:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+let’s install MAVSDK-Python:
+[pip3 install mavsdk]
+
+install the lightweight package called “aioconsole”, which provides apython
+[pip3 install aioconsole]
+
+start SITL
+[make px4_sitl_default jmavsim]
+
+node install
+[npm install]
+[]npm start]
+
+goto link to takeoff
+http://localhost:3000/takeoff
+
---
+++ start.js
@@ -0,0 +1,19 @@
+var express = require('express'); 
+var app = express(); 
+  
+
+app.listen(3000, function() { 
+    console.log('server running on port 3000'); 
+} ) 
+  
+app.get('/takeoff', callName); 
+  
+function callName(req, res) { 
+  
+    var spawn = require("child_process").spawn; 
+    var process = spawn('python3',["./takeoff_and_land.py", 
+                           ] ); 
+     process.stdout.on('data', function(data) { 
+        res.send(data.toString()); 
+    } ) 
+} 
