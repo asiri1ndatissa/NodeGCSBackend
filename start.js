6b30daea8ed28d2329edef25a079d2ca18aa05c4
Xasiri <asiri1234@gmail.com>
2021-02-08 08:40:22 +0530

    mission.py

--- start.js
+++ start.js
@@ -17,3 +17,15 @@ function callName(req, res) {
         res.send(data.toString()); 
     } ) 
 } 
+
+app.get('/mission', callName); 
+  
+function callName(req, res) { 
+  
+    var spawn = require("child_process").spawn; 
+    var process = spawn('python3',["./mission.py", 
+                           ] ); 
+     process.stdout.on('data', function(data) { 
+        res.send(data.toString()); 
+    } ) 
+} 
