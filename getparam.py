1c6dbeaf4b0318f46ee2ea2f09c75ff04e905403
Yuthika Sagarage <sandamalsagarage@gmail.com>
2021-02-08 18:06:19 +0530

    clean up

--- getparam.py
+++ getparam.py
@@ -1,71 +1,29 @@
-
-# import asyncio
-# from mavsdk import System
-# from mavsdk.param import (IntParam,FloatParam,AllParams,ParamResult,ParamError,Param)
-
-
-
-# async def run():
-#     drone = System()
-#     await drone.connect(system_address="udp://:14540")
-
-    
-#     print("drone to conaaaaaaaaanect...")
-#     # params= Param.get_all_params()
-
-#     async for param in drone.param.get_all_params():
-#         print(f"health: {param}")
-    
-
-
-# if __name__ == "__main__":
-#     loop = asyncio.get_event_loop()
-#     loop.run_until_complete(run())
-
 import asyncio
 from mavsdk import System
 from mavsdk.param import Param
 from operator import attrgetter
+
 async def run():
     # Init the drone
     drone = System()
-   
-  
+     
     await drone.connect(system_address="udp://:14540")
-    
-    # Start the tasks
-    asyncio.ensure_future(print_battery(drone))
-    # asyncio.ensure_future(print_gps_info(drone))
-    # asyncio.ensure_future(print_in_air(drone))
-    # asyncio.ensure_future(print_position(drone))
-   
-async def print_battery(drone):
-    x = await drone.param.get_all_params()
-    gender, username = attrgetter('int_params', 'float_params')(x)
+        # Start the tasks
+    asyncio.ensure_future(print_params(drone))
 
-    # y = attrgetter('<mavsdk.param.IntParam object at 0x7febcc219cc0>')(gender)
-    for ele in enumerate(gender):
+   
+async def print_params(drone):
+    x = await drone.param.get_all_params()
+    intparams, float_params = attrgetter('int_params', 'float_params')(x)
+
+    for ele in enumerate(intparams):
+      name , value = attrgetter('name', 'value')(ele[1])
+      print(name, value)
+    for ele in enumerate(float_params):
       name , value = attrgetter('name', 'value')(ele[1])
       print(name, value)
 
-        
-  
-async def print_gps_info(drone):
-    async for gps_info in drone.telemetry.gps_info():
-        print(f"GPS info: {gps_info}")
-
-        
-
-async def print_in_air(drone):
-    async for in_air in drone.telemetry.in_air():
-        print(f"In air: {in_air}")
-
-
-async def print_position(drone):
-    async for position in drone.telemetry.position():
-        print(position)
-
-
+          
 if __name__ == "__main__":
     # Start the main function
     asyncio.ensure_future(run())
